8567643fa2446ee52aca6a5fb7011be96fb99e73
JOSY12 <josmer1997@hotmail.es>
2022-12-02 16:12:01 -0500

    d

--- src/components/dashboard/CreateProduct.jsx
+++ src/components/dashboard/CreateProduct.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
